772f0963142abfaf8e8b0233bb8e877ee66b283e
Rafael Godoy Guía <rgodoyguia@gmail.com>
2024-10-17 09:53:02 +0200

    hola

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -4,7 +4,7 @@ import dominio.Localidad;
 
 public class Principal {
     public static void main (String[] args) {
-        Localidad localidad = new Localidad();
+        Localidad localidad = new Localidad(12345);
 
         System.out.println(localidad.toString());
     }
